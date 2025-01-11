2ebd95bb008e806ff2a3e56d2872c75d393bba0d
AbroShahzeb <khizr77@gmail.com>
2025-01-11 17:10:04 +0500

    Changes

--- src/pages/home/components/hero.jsx
+++ src/pages/home/components/hero.jsx
@@ -21,15 +21,15 @@ export const Hero = () => {
 
       {/* Hero Section */}
       <div className="mt-8 flex flex-col gap-6 px-6 md:gap-0 lg:px-0 lg:gap-[100px] lg:flex-row-reverse lg:max-w-[1110px] lg:mx-auto relative z-40 lg:items-center ">
-        <div className="flex items-center justify-center mx-auto lg:mt-[calc(74px+34px)] lg:ml-[20px]">
+        <div className="flex items-center justify-center mx-auto lg:mt-[calc(74px+34px)] ">
           <img
             src={illustrationPhoneMockup}
             alt="Phone Mockup Illustration"
-            className="ml-4 lg:h-[650px] lg:w-auto lg:-my-[125px]"
+            className="ml-4 lg:h-[650px] lg:w-auto lg:-my-[125px] lg:ml-[20px]"
           />
         </div>
 
-        <div className="flex flex-col lg:items-start gap-6 md:gap-0 md:mt-12 lg:mt-0">
+        <div className="flex flex-col lg:items-start gap-6 md:gap-0 md:mt-12">
           <h1 className="text-[32px] leading-[36px] font-dm-serif text-center text-secondary-san-juan-blue md:text-[48px] lg:text-[72px] lg:leading-[72px] lg:tracking-[-0.554px] lg:text-left md:max-w-[537px] md:leading-[56px] md:tracking-[-0.369px] md:mx-auto">
             Start building with our APIs for absolutely free.
           </h1>
@@ -56,7 +56,7 @@ export const Hero = () => {
             </div>
             <p className="text-secondary-light-san-juan-blue text-body-copy-1 text-center lg:text-left">
               Have any question?{" "}
-              <Link className="font-bold" to="/contact">
+              <Link className="font-bold underline" to="/contact">
                 Contact Us
               </Link>
             </p>
