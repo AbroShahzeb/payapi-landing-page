a74bb28436e1fcb01e327b054b1548020b9aa8f7
AbroShahzeb <khizr77@gmail.com>
2025-01-11 14:09:53 +0500

    About Us Desktop Version Done

--- src/layout/navbar/index.jsx
+++ src/layout/navbar/index.jsx
@@ -6,7 +6,7 @@ import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-6 py-10 lg:pt-[45px] relative z-30 lg:max-w-[1110px] lg:mx-auto md:px-10">
+    <nav className="flex items-center justify-between px-6 py-10 lg:pt-[45px] relative z-30 lg:max-w-[1110px] lg:mx-auto md:px-10 lg:px-0">
       <Link className="lg:mr-16" to="/">
         <img src={logo} alt="PayAPI logo" />
       </Link>
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -19,12 +19,14 @@ export const About = () => {
 
       <Navbar />
 
-      <div className="px-6 lg:max-w-[1110px] lg:mx-auto md:px-10">
-        <h1 className="text-[32px] leading-[36px] font-dm-serif tracking-[-0.246px] font-normal text-secondary-san-juan-blue text-center md:mt-10 lg:text-[56px] lg:leading-[56px] lg:text-left md:max-w-[573px] md:mx-auto md:text-[48px] md:leading-[56px] md:tracking-[-0.369px]">
-          We empower innovators by delivering access to the financial system
-        </h1>
+      <div className="px-6  md:px-10 lg:max-w-[920px] lg:mx-auto lg:px-0">
+        <div className="lg:max-w-[730px] lg:flex lg:items-start">
+          <h1 className="text-[32px] leading-[36px] font-dm-serif tracking-[-0.246px] font-normal text-secondary-san-juan-blue text-center md:mt-10 lg:text-[56px] lg:leading-[56px] lg:text-left md:max-w-[573px] md:mx-auto md:text-[48px] md:leading-[56px] md:tracking-[-0.369px] lg:max-w-full">
+            We empower innovators by delivering access to the financial system
+          </h1>
+        </div>
 
-        <div className="mt-10 flex flex-col gap-4 items-center text-center md:mt-[54px] md:flex-row md:items-start">
+        <div className="mt-10 flex flex-col gap-4 items-center text-center md:mt-[54px] md:flex-row md:items-start lg:mt-[50px]">
           <h3 className="text-[24px] leading-[32px] text-secondary-san-juan-blue font-dm-serif tracking-[-0.185px] font-normal md:w-[223px] md:text-left md:flex-shrink-0 md:gap-[10px]">
             Our Vision
           </h3>
@@ -37,7 +39,7 @@ export const About = () => {
           </p>
         </div>
 
-        <div className="mt-10 flex flex-col gap-4 items-center text-center md:mt-[64px] md:flex-row md:items-start">
+        <div className="mt-10 flex flex-col gap-4 items-center text-center md:mt-[64px] md:flex-row md:items-start lg:mt-[56px]">
           <h3 className="text-[24px] leading-[32px] text-secondary-san-juan-blue font-dm-serif tracking-[-0.185px] font-normal md:w-[223px] md:text-left md:flex-shrink-0 md:gap-[10px]">
             Our Business
           </h3>
@@ -50,59 +52,70 @@ export const About = () => {
         </div>
       </div>
 
-      <div>
+      <div className="relative">
         <img src={teamMemberMobile} className="md:hidden mt-[76px] w-full" />
         <img
           src={teamMemberTablet}
           className="hidden md:block lg:hidden w-full mt-[90px]"
         />
-        <img src={teamMemberDesktop} className="hidden lg:block w-full" />
+        <img
+          src={teamMemberDesktop}
+          className="hidden lg:block w-full mt-[100px] relative z-20"
+        />
+
+        <div className="absolute top-1/2 left-0 -translate-y-1/2 -translate-x-1/2 z-10 hidden lg:block">
+          <img
+            src={bgPatterCircle}
+            alt="Background Circle Pattern"
+            className="w-[780px] h-[780px]"
+          />
+        </div>
       </div>
 
-      <div className="px-6 md:px-10">
+      <div className="px-6 md:px-10 lg:px-0 lg:max-w-[1110px] lg:mx-auto">
         {/* SEPARATOR */}
-        <div className="w-full h-[1px] bg-secondary-san-juan-blue  mt-12 md:hidden"></div>
+        <div className="w-full h-[1px] bg-secondary-san-juan-blue/25  mt-12 md:hidden"></div>
 
-        <div className="flex flex-col gap-8 items-center text-center py-4 md:flex-row md:gap-[10px] md:py-0">
+        <div className="flex flex-col gap-8 items-center text-center py-4 md:flex-row md:gap-[10px] md:py-0 lg:gap-[30px]">
           <div className="flex flex-col items-center text-center md:flex-1 md:text-left md:items-start">
-            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue  mt-12 mb-[44px]"></div>
+            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue/25  mt-12 mb-[44px] lg:mb-4 lg:mt-[72px]"></div>
             <p className="text-base leading-[28px] text-secondary-san-juan-blue/70">
               Team Members
             </p>
             <h4 className="text-[56px] leading-[72px] tracking-[-0.431px] text-primary-pink font-dm-serif">
               300+
             </h4>
-            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue  mt-4"></div>
+            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue/25  mt-4"></div>
           </div>
 
           <div className="flex flex-col items-center text-center md:flex-1 md:text-left md:items-start">
-            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue  mt-12 mb-[44px]"></div>
+            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue/25  mt-12 mb-[44px] lg:mb-4 lg:mt-[72px]"></div>
             <p className="text-base leading-[28px] text-secondary-san-juan-blue/70">
               Offices in the US
             </p>
             <h4 className="text-[56px] leading-[72px] tracking-[-0.431px] text-primary-pink font-dm-serif">
               3
             </h4>
-            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue  mt-4"></div>
+            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue/25  mt-4"></div>
           </div>
 
           <div className="flex flex-col items-center text-center md:flex-1 md:text-left md:items-start">
-            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue  mt-12 mb-[44px]"></div>
+            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue/25  mt-12 mb-[44px] lg:mb-4 lg:mt-[72px]"></div>
             <p className="text-base leading-[28px] text-secondary-san-juan-blue/70">
               Transactions analyzed
             </p>
             <h4 className="text-[56px] leading-[72px] tracking-[-0.431px] text-primary-pink font-dm-serif">
               10M+
             </h4>
-            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue  mt-4"></div>
+            <div className="hidden md:block w-full h-[1px] bg-secondary-san-juan-blue/25  mt-4"></div>
           </div>
         </div>
 
         {/* SEPARATOR */}
-        <div className="w-full h-[1px] bg-secondary-san-juan-blue  mb-12 md:hidden"></div>
+        <div className="w-full h-[1px] bg-secondary-san-juan-blue/25  mb-12 md:hidden"></div>
       </div>
 
-      <div className="px-6 lg:max-w-[1110px] lg:mx-auto mb-12 md:px-10 md:mb-[72px]">
+      <div className="px-6 lg:max-w-[920px] lg:mx-auto mb-12 md:px-10 md:mb-[72px] lg:px-0 lg:mb-[80px]">
         <div className="mt-10 flex flex-col gap-4 items-center text-center md:mt-[64px] md:flex-row md:items-start">
           <h3 className="text-[24px] leading-[32px] text-secondary-san-juan-blue font-dm-serif tracking-[-0.185px] font-normal md:w-[223px] md:text-left md:flex-shrink-0 md:gap-[10px]">
             The Culture
